65faed380d958b73a342faae61dfaf1311f7dbf8
Remy D. Farley <one-d-wide@protonmail.com>
2026-01-27 17:50:58 +0000

    scripts/regenerate.sh: run only when needed

--- scripts/regenerate.sh
+++ scripts/regenerate.sh
@@ -2,21 +2,55 @@
 
 set -e
 
+force=
+changed=
+
+if [ $# -ne 0 ]; then
+  force=1
+fi
+
 if ! test -f netlink-bindings/src/lib.rs; then
   echo "Should be run from project root"
   exit 1
 fi
 
-for subsystem in netlink-bindings/src/*/; do
-  cargo run --bin codegen -- -d "$subsystem"
+codegen="$(cargo run --bin codegen --config 'target."cfg(true)".runner="echo"')"
+
+for subsys in netlink-bindings/src/*/; do
+  subsys_name="$(basename -- "$subsys")"
+  yaml="$subsys/$subsys_name.yaml"
+  overrides_yaml="$subsys/$subsys_name.overrides.yaml"
+  mod="$subsys/mod.rs"
+  dump="$subsys/$subsys_name.md"
+
+  if [ -z "$force" ] &&
+    [ -e "$mod" ] &&
+    [ "$mod" -nt "$codegen" ] &&
+    [ "$mod" -nt "$yaml" ] &&
+    [ ! -e "$overrides_yaml" -o "$mod" -nt "$overrides_yaml" ]; then
+    echo Skipping "$subsys_name"
+    continue
+  fi
+
+  args=""
+  case "$subsys_name" in
+  # For most subsystems, dump files get too large to be useful
+  nlctrl | wireguard)
+    args="$args --dump $dump"
+    ;;
+  esac
+
+  echo Generating "$subsys_name"
+  command "$codegen" -d "$subsys" $args
+
+  changed=1
 done
 
-# For most subsystems, dump files get too large to be useful
-for subsys in nlctrl wireguard; do
-  subsys_prefix="netlink-bindings/src/$subsys/$subsys"
-  cargo run --bin codegen -- \
-    "${subsys_prefix}.yaml" \
-    --dump "${subsys_prefix}.md"
-done
+gen="./reverse-lookup/src/generated.rs"
 
-cargo run --bin codegen -- -d ./netlink-bindings/src --reverse-lookup ./reverse-lookup/src/generated.rs
+if [ -n "$force" ] || [ ! -e "$gen" ] || [ -n "$changed" ]; then
+  echo Generating reverse-lookup
+  command "$codegen" -d ./netlink-bindings/src --reverse-lookup "$gen"
+else
+  echo Skipping reverse-lookup
+fi
